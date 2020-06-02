958c7e3a31f43d471d5732d3f4d1756643f364fe
Nilton Silva <nsfgeo@hotmail.com>
2020-06-02 18:08:15 -0300

    Aula 11 (Variáveis - Tipos Primitivos)

---
+++ curso-java-basico/src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,17 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 33;
+		String nome = "Nilton";
+		String nomeDoMeuCachorro = "Madruguinha";
+		
+		System.out.println("Idade: " + idade);
+		System.out.println("Nome: " + nome);
+		System.out.println("Nome do meu cahorro: " + nomeDoMeuCachorro);
+
+	}
+
+}
