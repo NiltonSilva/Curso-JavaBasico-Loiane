96d08dac36721576a611227de141afde0f224f4c
Nilton Silva <nsfgeo@hotmail.com>
2020-06-10 22:12:01 -0300

    Aulas 19 e 20 (Matrizes irregulares)

---
+++ curso-java-basico/src/com/loiane/cursojava/aula20/MatrizIrregular.java
@@ -0,0 +1,36 @@
+package com.loiane.cursojava.aula20;
+
+import java.util.Scanner;
+
+public class MatrizIrregular {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Entre com o número de pessoas que serão intrevistadas: ");
+		int numeroEntrevistados = scan.nextInt();
+		
+		String[][] nomesFilhos = new String[numeroEntrevistados][];
+		
+		for(int i = 0; i < nomesFilhos.length; i++) {
+			
+			System.out.print("Qual a quantidade de filhos? ");
+			int qtdFilhos = scan.nextInt();
+			
+			nomesFilhos[i] = new String[qtdFilhos];
+			
+			for(int j = 0; j < nomesFilhos[i].length; j++) {
+				System.out.print("Digite o nome do filho " + (j+1) + ": ");
+				nomesFilhos[i][j] = scan.next();
+			}
+		}
+		
+		for(int i = 0; i < nomesFilhos.length; i++) {
+			System.out.println("Pessoa " + (i+1) + " tem " + nomesFilhos[i].length + " filhos");
+			for(int j = 0; j < nomesFilhos[i].length; j++) {
+				System.out.println(nomesFilhos[i][j]);
+			}	
+		}
+	}
+}
