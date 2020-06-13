d8ddf01aff900e4abf73c85949902f9c2c99f3b8
Nilton Silva <nsfgeo@hotmail.com>
2020-06-13 11:36:21 -0300

    Aulas 25(metodos de classe sem retorno), 26 (metodos de classe com retorno) e 27 (metodos de classe com retorno e passagem de parametros)

---
+++ curso-java-basico/src/com/loiane/cursojava/aula26/Carro.java
@@ -0,0 +1,19 @@
+package com.loiane.cursojava.aula26;
+
+public class Carro {
+	
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+	
+	void exibirAutonomia() {
+		System.out.println("A autonomia do carro é: " + capCombustivel * consumoCombustivel + " km.");
+	}
+	
+	double obterAutonomia() {
+		System.out.println("Método obter autonomia foi chamado com suceso.");
+		return capCombustivel * consumoCombustivel;
+	}
+}
